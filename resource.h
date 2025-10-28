631cc9e7f55bfcc4f35bda33848823fa3f359504
Agzes <103037173+Agzes@users.noreply.github.com>
2025-10-28 18:08:56 +1200

    🚀 • AntiAFK-RBX V3

--- resource.h
+++ resource.h
@@ -2,11 +2,7 @@
 // Microsoft Visual C++ generated include file.
 // Used by AntiAFK-RBX.rc
 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
+#define IDI_MAIN                        101
+#define IDI_TRAY_OFF                    102
+#define IDI_TRAY_ON                     103
+#define IDI_TRAY_ON_MULTI               104
